77d1784c7415e4123f7b8831f7ad35cf920af832
jeon-ji-yoon <appleru1515@gmail.com>
2021-02-23 19:11:21 +0900

    스프링 데이터 Jpa 활용

--- src/main/java/hello/hellodesperate/SpringConfig.java
+++ src/main/java/hello/hellodesperate/SpringConfig.java
@@ -1,40 +1,38 @@
 package hello.hellodesperate;
 
-import hello.hellodesperate.repository.JdbcMemberRepository;
-import hello.hellodesperate.repository.JpaMemberRepository;
 import hello.hellodesperate.repository.MemberRepository;
 import hello.hellodesperate.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
-
 @Configuration //아래의 것들을 스프링 빈에 등록하라는 뜻이네?
 public class SpringConfig {
 
-    private EntityManager em;
+    private final MemberRepository memberRepository; //springDataJpaMemberRepository
+    //jparepository 가 해당 인터페이스를 자동으로 빈에 등록해줌...
 
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
-
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
-
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+
+        return new MemberService(memberRepository);
     }
+    /*
     @Bean
     public MemberRepository memberRepository() {
 
-        return new JpaMemberRepository(em);
+
+        return
+        //return new JpaMemberRepository(em);
 
         //return new JdbcMemberRepository(dataSource);
     }
-
+*/
 
 
 }
